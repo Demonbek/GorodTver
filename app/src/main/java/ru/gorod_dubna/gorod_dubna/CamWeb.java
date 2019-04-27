514d841fea2682e571ead89a1ed79f8b5c9e06fd
DemonApps <demon.bek@gmail.com>
2019-04-27 19:23:26 +0300

    Сделал камеры, в меню новостей добавил погоду и вакансии версия 17

---
+++ app/src/main/java/ru/gorod_dubna/gorod_dubna/CamWeb.java
@@ -0,0 +1,128 @@
+package ru.gorod_dubna.gorod_dubna;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.RelativeLayout;
+
+import com.my.target.ads.MyTargetView;
+
+
+public class CamWeb extends AppCompatActivity {
+    String url = "file:///android_asset/webCam1.html";
+    WebView camWeb;
+    private MyTargetView adViewWebcam;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_web);
+        final RelativeLayout layout = findViewById(R.id.activityLayoutWebcam);
+        camWeb = findViewById(R.id.camWeb);
+
+        camWeb.getSettings().setJavaScriptEnabled(true);
+        camWeb.getSettings().setUseWideViewPort(true);
+        camWeb.getSettings().setLoadWithOverviewMode(true);
+        camWeb.loadUrl(url);
+// Включение режима отладки
+        //MyTargetView.setDebugMode(true);
+
+        // Создаем экземпляр MyTargetView, формат 320х50
+        adViewWebcam = new MyTargetView(this);
+
+        // Создаем экземпляр MyTargetView, формат 300х250
+        // adView = new MyTargetView(this, AdSize.BANNER_300x250);
+
+        // Инициализируем экземпляр
+        adViewWebcam.init(416740);
+
+
+        // Устанавливаем слушатель событий
+        adViewWebcam.setListener(new MyTargetView.MyTargetViewListener() {
+            @Override
+            public void onLoad(@NonNull MyTargetView myTargetView) {
+                // Данные успешно загружены, запускаем показ объявлений
+                layout.addView(adViewWebcam);
+            }
+
+            @Override
+            public void onNoAd(@NonNull String reason, @NonNull MyTargetView myTargetView) {
+            }
+
+            @Override
+            public void onClick(@NonNull MyTargetView myTargetView) {
+            }
+        });
+
+        // Запускаем загрузку данных
+        adViewWebcam.load();
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_webcam, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // получим идентификатор выбранного пункта меню
+        int id = item.getItemId();
+        // Операции для выбранного пункта меню
+        switch (id) {
+            case R.id.action_webcan1:
+                camWeb.loadUrl("file:///android_asset/webCam1.html");
+                setTitle(R.string.action_webcam1);
+                return true;
+            case R.id.action_webcan2:
+                camWeb.loadUrl("file:///android_asset/webCam2.html");
+                setTitle(R.string.action_webcam2);
+                return true;
+            case R.id.action_webcan3:
+                camWeb.loadUrl("file:///android_asset/webCam3.html");
+                setTitle(R.string.action_webcam3);
+                return true;
+            case R.id.action_webcan4:
+                camWeb.loadUrl("file:///android_asset/webCam4.html");
+                setTitle(R.string.action_webcam4);
+                return true;
+            case R.id.action_webcan5:
+                camWeb.loadUrl("file:///android_asset/webCam5.html");
+                setTitle(R.string.action_webcam5);
+                return true;
+            case R.id.action_webcan6:
+                camWeb.loadUrl("file:///android_asset/webCam6.html");
+                setTitle(R.string.action_webcam6);
+                return true;
+            case R.id.action_webcan7:
+                camWeb.loadUrl("file:///android_asset/webCam7.html");
+                setTitle(R.string.action_webcam7);
+                return true;
+            case R.id.action_webcan8:
+                camWeb.loadUrl("file:///android_asset/webCam8.html");
+                setTitle(R.string.action_webcam8);
+                return true;
+            case R.id.action_webcan9:
+                camWeb.loadUrl("file:///android_asset/webCam9.html");
+                setTitle(R.string.action_webcam9);
+                return true;
+            case R.id.action_webcan10:
+                camWeb.loadUrl("file:///android_asset/webCam10.html");
+                setTitle(R.string.action_webcam10);
+                return true;
+            case R.id.action_webcan11:
+                camWeb.loadUrl("file:///android_asset/webCam11.html");
+                setTitle(R.string.action_webcam11);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/ru/gorod_dubna/gorod_dubna/Zastavka.java
@@ -0,0 +1,42 @@
+package ru.gorod_dubna.gorod_dubna;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Zastavka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zastavka);
+
+        //Создаем новый поток:
+        Thread splash_time = new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    //Целое значение время отображения картинки:
+                    int SplashTimer = 0;
+                    //Запускаем цикл длиной в 3 секунды:
+                    while(SplashTimer < 3000) {
+                        sleep(100);
+                        SplashTimer = SplashTimer +100;
+                    }
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace(); }
+                finally {
+                    //Закрываем activity:
+                    finish();
+                    startActivity(new Intent(Zastavka.this,MainActivity.class));
+                }
+            }
+        };
+        splash_time.start();
+
+    }
+}
+
